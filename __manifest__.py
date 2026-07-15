2584280ff692d8c77e241207441f4ad6d905cf12
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-15 18:02:42 -0500

    STYLE: Unificar diseño con tema claro nativo de Odoo y usar clase oe_highlight en botones del header (v18.0.1.0.11)

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     'support': "ventas@mbaconsultings.com",
     'license': "AGPL-3",
     'category': 'Sales',
-    'version': '18.0.1.0.10',
+    'version': '18.0.1.0.11',
     'depends': ['base', 'sale', 'mail'],
     'data': [
         'security/security.xml',
